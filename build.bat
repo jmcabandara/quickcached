a4196d5f4f36a3bf6f26c840305bb02bb296aaa3
akshath <akshath@gmail.com>
2011-01-27 06:39:39 +0000

    updated

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 @echo off
 set JAVA_HOME=g:\jdk1.5.0_16
-set JAVA=%JAVA_HOME%\bin\java
+set ANT_HOME=G:\apache-ant-1.8.2
 
-set cp=%JAVA_HOME%\lib\tools.jar;.\lib\ant.jar;
-%JAVA% -classpath "%CP%" -Dant.home=lib org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 -buildfile build.xml
+set PATH=%JAVA_HOME%\bin;%PATH%
+set CLASSPATH=%JAVA_HOME%\lib\tools.jar;.;
+
+%ANT_HOME%\bin\ant %1 %2 %3 %4 %5 %6 -buildfile build.xml
