76aa390bce7a44fc9159f2f6da4a3be3822dace7
akshath@gmail.com <akshath@gmail.com@e35125e7-12c9-c220-3669-c5f0d15dcf30>
2011-02-06 15:10:22 +0000

    updated

--- trunk/QuickCached.sh
+++ trunk/QuickCached.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cp1=./lib/whirlycache-1.0.1.jar:./lib/concurrent.jar:./lib/QuickServer.jar:./lib/log4j.jar
-java -server -cp $cp1 -Xms50m -Xmx512m -XX:CompileThreshold=1500 -Dnet.spy.log.LoggerImpl=net.spy.memcached.compat.log.SunLogger -Xconcurrentio org.quickserver.net.server.QuickServer -load conf/QuickCached.xml
+java -server -Xms50m -Xmx512m -XX:CompileThreshold=1500 -Xconcurrentio -jar dist/QuickCached.jar $@
