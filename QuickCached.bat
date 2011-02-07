79be6490ee26fbbf2bb2179506602b99e74f5a97
akshath <akshath@gmail.com>
2011-02-07 17:24:57 +0000

    updated

--- QuickCached.bat
+++ QuickCached.bat
@@ -1,9 +1,7 @@
 @echo off
-set JAVA_HOME=g:\jdk1.5.0_16
-set JAVA=%JAVA_HOME%\bin\java
+rem set JAVA_HOME=g:\jdk1.5.0_16
+rem set JAVA=%JAVA_HOME%\bin\java
+rem set cp1=%JAVA_HOME%\lib\tools.jar;.;
+rem -cp %cp1% 
 
-set cp1=%JAVA_HOME%\lib\tools.jar;.;
-
-set cp1=%cp1%;
-
-@java -server -cp %cp1% -Xms50m -Xmx512m  -XX:CompileThreshold=1500 -Xconcurrentio -jar dist\QuickCached.jar %*
+@java -server -Xms50m -Xmx512m  -XX:CompileThreshold=1500 -Xconcurrentio -jar dist\QuickCached.jar %*
