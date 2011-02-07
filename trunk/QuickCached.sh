a01ef8cdab2f3a5c9e207d9ab36dc0bd474c5fbf
akshath@gmail.com <akshath@gmail.com@e35125e7-12c9-c220-3669-c5f0d15dcf30>
2011-02-07 17:24:57 +0000

    updated

--- trunk/QuickCached.sh
+++ trunk/QuickCached.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -server -Xms50m -Xmx512m -XX:CompileThreshold=1500 -Xconcurrentio -jar dist/QuickCached.jar $@
+exec java -server -Xms50m -Xmx512m -XX:CompileThreshold=1500 -Xconcurrentio -jar dist/QuickCached.jar $@
