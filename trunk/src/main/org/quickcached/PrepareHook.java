962f0836b8b09eae1410cf8a11436e74a7fb4e06
akshath@gmail.com <akshath@gmail.com@e35125e7-12c9-c220-3669-c5f0d15dcf30>
2011-08-07 09:17:57 +0000

    + Added Stats Report support to log stats output to a csv file at a interval specified

--- trunk/src/main/org/quickcached/PrepareHook.java
+++ trunk/src/main/org/quickcached/PrepareHook.java
@@ -1,12 +1,18 @@
 package org.quickcached;
 
+import java.util.Arrays;
+import java.util.List;
 import org.quickserver.net.server.*;
 
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.quickserver.net.ServerHook;
 
 public class PrepareHook implements ServerHook {
+	private static final Logger logger = Logger.getLogger(PrepareHook.class.getName());
+	
 	private QuickServer quickserver;
 
 	public String info() {
@@ -23,6 +29,34 @@ public class PrepareHook implements ServerHook {
 			try {
 				CommandHandler.init(config);
 			} catch(Exception e) {
+				logger.log(Level.WARNING, "Error: "+e, e);
+			}
+
+			try {
+				String enableStatsReportStr = (String) config.get("ENABLE_STATS_REPORT");
+				boolean enableStatsReport = false;
+
+				if("true".equals(enableStatsReportStr)) {
+					enableStatsReport = true;
+				}
+
+				String statsReportWriteIntervalStr = (String) config.get("STATS_REPORT_WRITE_INTERVAL");
+				if(statsReportWriteIntervalStr!=null) {
+					StatsReportGenerator.setWriteInterval(Integer.parseInt(statsReportWriteIntervalStr));
+				}
+
+				String entriesToLog  = (String) config.get("ENTRIES_TO_LOG");
+				if(entriesToLog!=null) {
+					List entriesToLogList = Arrays.asList(entriesToLog.split(","));
+					entriesToLogList.remove(" ");
+					StatsReportGenerator.setEntriesToLog(entriesToLogList);
+				}
+
+				if(enableStatsReport) {
+					StatsReportGenerator.start(quickserver);
+				}
+			} catch(Exception e) {
+				logger.log(Level.WARNING, "Error: "+e, e);
 				e.printStackTrace();
 			}
 			return true;
