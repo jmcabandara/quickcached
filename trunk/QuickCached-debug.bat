3bf3518da962153083b66295f0a1d1fdb34fea76
akshath <akshath@e35125e7-12c9-c220-3669-c5f0d15dcf30>
2013-02-06 12:23:43 +0000

    updated gc algo

--- trunk/QuickCached-debug.bat
+++ trunk/QuickCached-debug.bat
@@ -5,10 +5,10 @@ rem set JAVA_HOME=d:\jdk1.6.0_23
 rem set JAVA=%JAVA_HOME%\bin\java
 rem set cp1=-cp %JAVA_HOME%\lib\tools.jar;.;
 
-set GC_OPTIONS=-verbose:gc -Xloggc:verbose_gc.log -XX:+PrintGCDetails -XX:+PrintHeapAtGC -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintClassHistogram   -XX:+PrintCommandLineFlags -XX:+PrintConcurrentLocks -XX:+PrintGCTimeStamps 
+set GC_OPTIONS=-verbose:gc -Xloggc:verbose_gc.log -XX:+PrintGCDetails -XX:+PrintHeapAtGC -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintClassHistogram -XX:+PrintCommandLineFlags -XX:+PrintConcurrentLocks -XX:+PrintGCTimeStamps 
 rem -XX:+PrintGCDateStamps -XX:+PrintTenuringDistribution  
 
-set JVM_OPTIONS=-XX:CompileThreshold=1500 -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:+UseParallelOldGCCompacting -XX:ParallelGCThreads=4
+set JVM_OPTIONS=-XX:CompileThreshold=1500 -XX:+UseConcMarkSweepGC
 
 rem %JAVA% %cp1% -server -Xms512m -Xmx512m %JVM_OPTIONS% %GC_OPTIONS% %EXTRA_OPTS% -jar dist\QuickCached-Server.jar %*
 java -server -Xms512m -Xmx512m %JVM_OPTIONS% %GC_OPTIONS% %EXTRA_OPTS% -jar dist\QuickCached-Server.jar %*
--- trunk/QuickCached.bat
+++ trunk/QuickCached.bat
@@ -5,7 +5,7 @@ rem set JAVA_HOME=d:\jdk1.6.0_23
 rem set JAVA=%JAVA_HOME%\bin\java
 rem set cp1=-cp %JAVA_HOME%\lib\tools.jar;.;
 
-set JVM_OPTIONS=-XX:CompileThreshold=1500 -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:+UseParallelOldGCCompacting -XX:ParallelGCThreads=4
+set JVM_OPTIONS=-XX:CompileThreshold=1500 -XX:+UseConcMarkSweepGC
 
 rem %JAVA% %cp1% -server -Xms512m -Xmx512m %JVM_OPTIONS% -Dappname=QC1 -jar dist\QuickCached-Server.jar %*
 java -server -Xms512m -Xmx512m %JVM_OPTIONS% -Dappname=QC1 -jar dist\QuickCached-Server.jar %*
